02f3be641529e744bbf24ea5ea20b7e8bc6850d9
Haneef Mubarak <haneefmubarak@users.noreply.github.com>
2013-12-29 14:00:11 -0800

    added functions

---
+++ libnix.c
@@ -0,0 +1,137 @@
+// lib*nix
+// the *nix library
+//
+// Copyright 2013 Haneef Mubarak
+// All Rights Reserved
+
+#include "libnix.h"
+
+int mmapFile (char *filename, mapFileStruct *mapfile) {
+	int  x;
+
+	struct stat statbuf;
+
+	mapfile->fd = -1;
+	if ((mapfile->fd = open (filename, mapfile->o_flag)) == -1) {
+		return -1;	// open failed
+	}
+
+	if (flock (mapfile->fd, mapfile->lock) == -1) {
+		do {
+			x = close (mapfile->fd);
+		} while ((x != 0) && (errno == EINTR));
+
+		return -2;	// locking failed
+	}
+
+	if (!fstat (mapfile->fd, &statbuf) == 0) {
+		do {
+			x = close (mapfile->fd);
+		} while ((x != 0) && (errno == EINTR));
+
+		return -3;	// fstat failed
+	}
+
+	mapfile->len = statbuf.st_size;
+	mapfile->addr = mmap (NULL, mapfile->len, mapfile->m_prot,
+				mapfile->m_flag, mapfile->fd,
+				mapfile->offset);
+	if (!(mapfile->addr)) {
+		do {
+			x = close (mapfile->fd);
+		} while ((x != 0) && (errno == EINTR));
+
+		return -4;	// mmap failed
+	}
+
+	return 0;
+}
+
+int munmapFile (mapFileStruct *mapfile) {
+	int x;
+
+	if (!munmap (mapfile->addr, mapfile->len)) {
+		return -1;	// munmap failed
+	}
+
+	do {
+		x = close (mapfile->fd);
+	} while ((x != 0) && (errno == EINTR));
+
+	return 0;
+}
+
+int mremapFile (off_t newSize, mapFileStruct *mapfile) {
+	int status = 0;
+
+	if (!ftruncate (mapfile->fd, newSize)) {
+		return -1;	// ftruncate failed
+	}
+
+	void *addr = mmap (NULL, newSize, mapfile->m_prot,
+				mapfile->m_flag, mapfile->fd,
+				mapfile->offset);
+	if (!addr) {
+		return -2;	// mmap new range failed
+	}
+
+	if (!munmap (mapfile->addr, mapfile->len)) {
+		status = 1;	// munmap old range failed
+	}
+
+	mapfile->addr = addr;
+	mapfile->len = newSize;
+
+	return status;
+}
+
+int getlines (char *string, char ***lines, size_t *linecount) {
+	int ptrcount = 256, countscale = 256;
+	char *start, *end;
+
+	size_t limit = strlen (string);
+
+	if (!(*lines = malloc (ptrcount * sizeof (char *)))) {
+		return 1;	// insufficient memory
+	}
+
+	start = string;
+	*linecount = 0;
+
+	while ((end = strchr (start, '\n'))) {
+		if (!((*lines)[*linecount] = strndup (start, (end - start)))) {
+			return 1;	// insufficient memory
+		}
+
+		(*linecount)++;
+		// Check that we have more memory
+		if (*linecount == ptrcount) {
+			// Make sure our scaling is fast enough
+			while ((ptrcount >= (countscale * 4)) && (countscale < 65536)) {
+				countscale = countscale * 4;
+			}
+
+			ptrcount += countscale;
+
+			// Make sure our realloc works
+			if (!(*lines = realloc (*lines, ptrcount * sizeof (char *)))) {
+				return 1;	//insufficient memory
+			}
+		}
+
+		// setup for round two
+		start = end + 1;
+	}
+
+	return 0;
+}
+
+int getSourceFile (mapFileStruct *mapfile, sourceFileStruct *sourcefile) {
+	sourcefile->file = mapfile;
+
+	char *data = mapfile->addr;
+	char ***lines = &sourcefile->line;
+	size_t *linecount = &sourcefile->linecount;
+
+	return getlines (data, lines, linecount);
+}
---
+++ libnix.h
@@ -0,0 +1,47 @@
+// lib*nix
+
+#pragma once
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <errno.h>
+
+#include <assert.h>
+
+#include <unistd.h>
+
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/file.h>
+
+#include <fcntl.h>
+
+#pragma pack(push,1)
+
+typedef struct {
+	void	*addr;
+	size_t	len;
+	int	m_prot;
+	int	m_flag;
+	int	o_flag;
+	int	lock;
+	int	fd;
+	off_t	offset;
+} mapFileStruct;
+
+typedef struct {
+	mapFileStruct	*file;
+	size_t	linecount;
+	char	**line;
+} sourceFileStruct;
+
+#pragma pack(pop)
+
+int mmapFile		(char *filename, mapFileStruct *mapfile);
+int mremapFile		(off_t newSize, mapFileStruct *mapfile);
+int munmapFile		(mapFileStruct *mapfile);
+
+int getlines		(char *string, char ***lines, size_t *linecount);
+int getSourceFile	(mapFileStruct *mapfile, sourceFileStruct *sourcefile);
